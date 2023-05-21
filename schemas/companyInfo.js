e889bab31684bec3315f6f66b37e5b244b6b7b05
Zachary Trundle <ztrunddev@gmail.com>
2023-05-21 09:06:37 -0500

    Add favicon and descriptions

--- schemas/companyInfo.js
+++ schemas/companyInfo.js
@@ -7,12 +7,27 @@ export default {
             name: 'companyName',
             title: 'Company Name',
             type: 'string',
+            description: 'The company name will be displayed prominently across the site. If a company logo is not provided, the company name will be used in its place in the header.',
             validation: (Rule) => Rule.required(),
         },
         {
             name: 'companyLogo',
             title: 'Company Logo',
             type: 'image',
+            description: 'Upload your company logo here. It will be used on the left side of the header across the site. If not provided, the company name will be used instead.',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'favicon',
+            title: 'Website Favicon',
+            type: 'image',
+            description: 'This image will be used as the website favicon. For best results, use a square image.',
+            options: {
+                hotspot: true,
+            },
+            validation: (Rule) => Rule.required(),
         },
     ]
 }
