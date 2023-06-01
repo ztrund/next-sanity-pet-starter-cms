7de3139f627bea75fe74ca24617c9a814d5202c6
Zachary Trundle <ztrunddev@gmail.com>
2023-05-31 23:01:21 -0500

    Add additional company info for pwa manifest

--- schemas/companyInfo.js
+++ schemas/companyInfo.js
@@ -11,6 +11,23 @@ export default {
             validation: (Rule) => Rule.required(),
         },
         {
+            name: 'companyNameShort',
+            title: 'Company Name (Short)',
+            type: 'string',
+            description: 'The short company name will be used when installed as a PWA on a mobile device. If not provided, the company name will be used instead.',
+            validation: (Rule) => [
+                Rule.required().error('Short company name is required'),
+                Rule.max(12).warning('Short company names should be 12 characters or less.'),
+            ],
+        },
+        {
+            name: 'companyDescription',
+            title: 'Company Description',
+            type: 'text',
+            description: 'The company description will be displayed when installing the site as a PWA on a mobile device.',
+            validation: (Rule) => Rule.required(),
+        },
+        {
             name: 'companyLogo',
             title: 'Company Logo',
             type: 'image',
@@ -23,7 +40,17 @@ export default {
             name: 'favicon',
             title: 'Website Favicon',
             type: 'image',
-            description: 'This image will be used as the website favicon. For best results, use a square image.',
+            description: 'This image will be used as the website favicon. The image should be square, ideally 192x192px or larger. Ensure all important content is placed within the center of the image. For best results, do not use a maskable icon for this purpose.',
+            options: {
+                hotspot: true,
+            },
+            validation: (Rule) => Rule.required(),
+        },
+        {
+            name: 'pwaIcon',
+            title: 'PWA Icon',
+            type: 'image',
+            description: 'This image will be used as the PWA icon. The image should be square, ideally 192x192px or larger. Ensure all important content is placed within a circular area in the center of the image, leaving some padding around the circle. This allows the icon to be displayed properly across different devices and platforms, which may apply various masks. You can use https://maskable.app/ to test and edit icon.',
             options: {
                 hotspot: true,
             },
